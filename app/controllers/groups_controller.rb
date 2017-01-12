37bdf52588f53d06ee8898412b4ac87e8e26bdad
ximigo1022 <liaohuatang1208@gmail.com>
2017-01-12 22:10:24 +0800

    add post button

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -47,7 +47,7 @@ class GroupsController < ApplicationController
   private
 
   def find_group_and_check_permission
-    @group = Group.find)(params[:id])
+    @group = Group.find(params[:id])
 
     if current_user != @group.user
       redirect_to root_path, alert: "You have no permission."
